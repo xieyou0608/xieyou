e87f89e93b6b961263ceb6d57a4a096829a5cbb0
xieyou0608 <b07705003@ntu.edu.tw>
2022-12-16 18:11:27 +0800

    [init] yarn create next app and add tailwind css.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    // fontFamily: {
+    //   sans: ["Graphik", "sans-serif"],
+    //   serif: ["Merriweather", "serif"],
+    // },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
